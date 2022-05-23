30b7493da85ea2538756ca4c19a51a2364251a14
timdean27 <timdean27@yahoo.com>
2022-05-23 13:36:35 -0400

    Working on Post route to creat new Project image and description

---
+++ src/Components/ProjectsDisplay/ShowProjectCards.js
@@ -0,0 +1,88 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const ShowProjectCards = (props) => {
+
+    const [newProjectForm, setNewProjectForm] = useState({
+        Projects:{
+
+            project:{
+                       image:"",
+                       shortVideo:"",
+                       Description:"",
+                       
+                     }
+        }
+    })
+
+    const handleChange = (event) => {
+        setNewProjectForm({...newProjectForm, [event.target.name]: event.target.value})
+        console.log("handle Change" ,newProjectForm)
+    }
+
+const handleSubNewProj = (event) => {
+        event.preventDefault();
+        props.createProject(newProjectForm);
+        setNewProjectForm({
+            Projects:{
+    
+                project:{
+                           image:"",
+                           shortVideo:"",
+                           Description:"",
+                           
+                         }
+            }
+        })
+        console.log("handleSubNewProj" ,newProjectForm)
+    }
+
+
+    const loadedProj = () => {
+        return props.frontEndData.map((dataRecived) => (
+                <div key={dataRecived._id} className='projectCard'>
+                <h2>Showing Prjects Obeject from ShowProjectCards</h2>
+                <h3>{dataRecived.Projects.project.Description}</h3>
+                <h3>{dataRecived.Projects.project.image}</h3>
+                <h3>{dataRecived.Projects.project.shortVideo}</h3>
+                </div>
+            )
+        )
+    }
+    const loadingProj = () => {
+        return <h1>Loading.........</h1>
+    }
+
+  return (
+      <div>
+      <form onSubmit={handleSubNewProj}>
+      <textarea
+          type='text'
+          value={newProjectForm.Projects.project.Description}
+          name='Projects.project.Description'
+          placeholder='Description'
+          onChange={handleChange}
+      />
+      <input
+          type='text'
+          value={newProjectForm.Projects.project.image}
+          name='Projects.project.image'
+          placeholder='image URL'
+          onChange={handleChange}
+      />
+      <input
+          type='text'
+          value={newProjectForm.Projects.project.shortVideo}
+          name='Projects.project.shortVideo'
+          placeholder='shortVideo URL'
+          onChange={handleChange}
+      />
+      <input type='submit' value='Add New Project' />
+  </form>
+
+      {props.frontEndData ? loadedProj() : loadingProj()}
+    </div>
+  )
+}
+
+export default ShowProjectCards
