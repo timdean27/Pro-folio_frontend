9a71ac32ed79779bff1a441953440db67ab299b5
dev-rome <haynesjerome@gmail.com>
2022-05-25 19:18:20 -0400

    fix errors

--- src/Components/MarketingRoutes.js
+++ src/Components/MarketingRoutes.js
@@ -1,62 +1,52 @@
-import React, { useState , useEffect} from 'react'
-import { Routes, Route} from 'react-router-dom';
-import ShowMarketing from '../Pages/Marketing/ShowMarketing';
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import ShowMarketing from "../Pages/Marketing/ShowMarketing";
 // test commit
 const MarketingRoutes = () => {
+  const [MarketingData, setMarketingData] = useState([]);
+  const urlMarketing = "http://localhost:4000/home/marketing";
 
-const [MarketingData, setMarketingData] = useState([])
-const urlMarketing = 'http://localhost:4000/home/marketing'
+  async function getMarketingData() {
+    await fetch(urlMarketing)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Marketing Data  insisde fetch funciton", data);
 
-async function getMarketingData() {
-  await fetch(urlMarketing).then((res) => res.json())
-    .then((data) => {
-      console.log('Marketing Data  insisde fetch funciton', data)
-
-      setMarketingData(data)
-    }).catch(console.error);
+        setMarketingData(data);
+      })
+      .catch(console.error);
   }
 
   const createMarketing = async (newMarketing) => {
     await fetch(urlMarketing, {
-        method: 'post',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newMarketing)
-    })
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newMarketing),
+    });
     getMarketingData();
-
-  }
-  
-  const createMarketing = async (newMarketing) => {
-    await fetch(urlMarketing, {
-        method: 'post',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newMarketing)
-    })
+  };
+  useEffect(() => {
     getMarketingData();
+  }, []);
+
+  if (!MarketingData) {
+    return (
+      <div>
+        <h2>Loading</h2>
+      </div>
+    );
   }
-    useEffect(() => {
-
-      getMarketingData()
-      }, []);
-   
-      if(!MarketingData) {
-          return( 
-            <div>
-              <h2>Loading</h2>
-            </div>
-          )
-        } 
-
 
   return (
     <div>
-    <ShowMarketing MarketingData ={MarketingData} createMarketing={createMarketing}/>
+      <ShowMarketing
+        MarketingData={MarketingData}
+        createMarketing={createMarketing}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default MarketingRoutes
+export default MarketingRoutes;
