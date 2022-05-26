646e7bec52b9bec7c65f5fb23119ecda376d8a2c
njivoon@gmail.com <njivoon@gmail.com>
2022-05-25 20:06:59 -0700

    organizingfilesfolders

--- src/Pages/Contact/ShowContact.js
+++ src/Pages/Contact/ShowContact.js
@@ -54,9 +54,7 @@ const ShowContact = ({ContactData, createContact}) => {
     return (
     <div className="contact-container">
       <h1>Contact</h1>
-      <section id="contact-section"></section> 
-      <form method="POST" name="contact"></form> 
-
+      <section id="contact-section"></section>
       {ContactData ? loadedContacts() : loadingContacts()}
       <form onSubmit={handleSubmit}>
 
