aad95add4865c6da3679ded9b78867b92a89a37f
timdean27 <timdean27@yahoo.com>
2022-05-25 20:40:23 -0400

    Saving PRogress on the Put forms for Projects page

--- src/Pages/Projects/SingleProjectView.js
+++ src/Pages/Projects/SingleProjectView.js
@@ -2,33 +2,78 @@ import React from 'react'
 import { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom'
 
-const SingleProjectView = ({ProjectsData}) => {
+const SingleProjectView = ({ProjectsData , updateProject}) => {
     const navigate = useNavigate()
     const { id } = useParams()
+    console.log("THIS IS THE IS",id)
+    console.log("THIS IS THE ProjectsData",ProjectsData)
+    const projectCurrent = () =>{ return ProjectsData.map((projects)=>(
+    projects.find(project => project._id === id)
+    ))}
     
+    console.log("projectCurrent",projectCurrent)
+
+    const [editProject, setEditProject] = useState(projectCurrent)
+
+
+
+    const handleChange = (event) => {
+        setEditProject({
+            project: {
+                ...editProject.project,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        updateProject(editProject, id)
+
+        navigate('/')
+    }
+
     const loadedProjects = () => {
-        return ProjectsData.map((data) => (
-                <div key={data._id} className='Projects'>
-                  <p>{data.project.Description}</p>
-                  <p>{data.project.image}</p>
-                  <p>{data.project.shortVideo}</p>
+        return (
+                <div key={projectCurrent._id} className='Projects'>
+                  <p>{projectCurrent.Description}</p>
+                  <p>{projectCurrent.image}</p>
+                  <p>{projectCurrent.shortVideo}</p>
                 </div>
             )
-        )
       }
       
       const loadingProjects = () => {
         return <h1>Loading.........</h1>
       }
 
-
-
-
-
-
   return (
     <div>SingleProjectView
     {ProjectsData ? loadedProjects() : loadingProjects()}
+    <form onSubmit={handleSubmit}>
+    <input
+        type='text'
+        value={editProject.project.image}
+        name='image'
+        placeholder='image URL'
+        onChange={handleChange}
+    />
+    <input
+        type='text'
+        value={editProject.project.shortVideo}
+        name='shortVideo'
+        placeholder='shortVideo URL'
+        onChange={handleChange}
+    />
+    <input
+        type='text'
+        value={editProject.project.Description}
+        name='Description'
+        placeholder='Description'
+        onChange={handleChange}
+    />
+    <input type='submit' value='Edit Project' />
+    </form>
     </div>
   )
 }
